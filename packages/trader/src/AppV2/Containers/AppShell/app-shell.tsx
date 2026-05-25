81e997cf90e828e09bc84287b50a30d1bfa06707
gsocietyy <shaloryan21@gmail.com>
2026-05-25 23:15:48 +0300

    removed side bar

--- packages/trader/src/AppV2/Containers/AppShell/app-shell.tsx
+++ packages/trader/src/AppV2/Containers/AppShell/app-shell.tsx
@@ -4,9 +4,6 @@ import { observer } from 'mobx-react-lite';
 
 import { routes } from '@deriv/shared';
 import { useStore } from '@deriv/stores';
-import { useDevice } from '@deriv-com/ui';
-
-import Sidebar from 'AppV2/Components/Layout/Sidebar/sidebar';
 
 import Router from '../../Routes/router';
 
@@ -15,7 +12,6 @@ import './app-shell.scss';
 const AppShell = observer(() => {
     const { ui } = useStore();
     const { active_sidebar_flyout } = ui;
-    const { isMobile } = useDevice();
     const location = useLocation();
 
     React.useEffect(() => {
@@ -26,7 +22,6 @@ const AppShell = observer(() => {
 
     return (
         <div className='app-shell'>
-            {!isMobile && <Sidebar />}
             <div className='app-shell__main-content'>
                 <Router />
             </div>
